98842649bc3e87bd39e44572272ac1d3027cbbf9
SchettlerKoehler <62466958+SchettlerKoehler@users.noreply.github.com>
2020-11-22 15:12:42 +0100

    Improved runForAllPullRequests (#2)
    * Workflow test
    
    * Build for all PRs
    
    * install fs-extra
    
    * Debug messages
    
    * Debug messages
    
    * Added headers
    
    * Added fs
    
    * Fixed error
    
    * Debug messages
    
    * Debug messages
    
    * Fixed error
    
    * Fixed error
    
    * Fixed error
    
    * Debug messages
    
    * Added user
    
    * Checkout only if needed
    
    * Debug messages
    
    * Checkout only if needed
    
    * Debug messages
    
    * Debug messages
    
    * Changed copying
    
    * Updated tutorials
    
    * Changed commit
    
    * Only commit changes
    
    * Trigger build
    
    * Updated tutorials
    
    * Removed push
    
    * Added push
    
    * Updated tutorials
    
    * Add schedule
    
    * Updated tutorials
    
    * Updated tutorials
    
    * Run other PRs even if one has an error
    
    * Fixed deletion of old folders
    
    * Updated tutorials
    
    Co-authored-by: devonfw <devonfw>

--- runForAllPullRequests.js
+++ runForAllPullRequests.js
@@ -33,36 +33,43 @@ function download(url, f) {
 download('https://api.github.com/repos/devonfw-forge/tutorials/pulls', function (data) {
     let json = JSON.parse(data);
     console.log(json);
+    let exitCode = 0;
 
     let repoDir = "repo/";
     let reposDirs = fs.readdirSync(repoDir);
     for (let index in reposDirs) {
-        if (/^[0-9]+/.test(reposDirs[index])) {
-            rimraf(repoDir + reposDirs[index], function () { console.log(repoDir + reposDirs[index] + " deleted"); });
+        if (/^[0-9]+_.+$/.test(reposDirs[index])) {
+            rimraf.sync(repoDir + reposDirs[index]);
         }
     }
+
     for (var i in json) {
         var e = json[i];
         console.log("Clone " + e.head.repo.clone_url + " -> " + e.head.ref);
         let cp = child_process.spawnSync("rm -R playbooks && git clone " + e.head.repo.clone_url + " playbooks && branch_name=$(git symbolic-ref -q HEAD) && branch_name=${branch_name##refs/heads/} && branch_name=${branch_name:-HEAD} && echo $branch_name && if [ \"$branch_name\" != \"" + e.head.ref + "\" ]; then git checkout " + e.head.ref + "; fi && sh buildRun.sh", { shell: true, encoding: 'utf-8' });
         console.log(cp);
         if (cp.status != 0) {
-            process.exit(cp.status);
+            exitCode = cp.status;
         }
+        else {
+            let katacodaDir = "build/output/katacoda/";
+            let tutorialDirs = fs.readdirSync(katacodaDir);
+            for (let index in tutorialDirs) {
+                let dir = katacodaDir + tutorialDirs[index];
+                let targetDir = "repo/" + e.number + "_" + e.title.replace(/[^A-Za-z0-9]/g, "-") + "_" + tutorialDirs[index];
+                console.log("Copy " + dir + " -> " + targetDir);
+                fse.copySync(dir, targetDir);
+            }
+        }
+        rimraf.sync("build/output/katacoda/");
+    }
 
-        let katacodaDir = "build/output/katacoda/";
-        let tutorialDirs = fs.readdirSync(katacodaDir);
-        for (let index in tutorialDirs) {
-            let dir = katacodaDir + tutorialDirs[index];
-            let targetDir = "repo/" + e.number + "_" + e.title.replace(/[^A-Za-z0-9]/g, "-") + "_" + tutorialDirs[index];
-            console.log("Copy " + dir + " -> " + targetDir);
-            fse.copySync(dir, targetDir);
-        }
-    } 
     let cp = child_process.spawnSync("cd repo && ls -al && git fetch --all && git checkout main && git add -A && git diff-index --quiet HEAD -- || (git config user.email \"devonfw\" && git config user.name \"devonfw\" && git commit -m \"Updated tutorials\" && git push)", { shell: true, encoding: 'utf-8' });
     console.log(cp);
     if (cp.status != 0) {
         process.exit(cp.status);
     }
-
+    if (exitCode != 0) {
+        process.exit(exitCode);
+    }
 });
