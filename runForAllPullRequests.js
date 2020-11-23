e30fd927321c16a3f5a016af6479772e19c29551
Jan-Gerrit Schettler-Köhler <jan-gerrit.schettler-koehler@capgemini.com>
2020-11-23 18:04:11 +0100

    Change folder

--- runForAllPullRequests.js
+++ runForAllPullRequests.js
@@ -46,19 +46,20 @@ download('https://api.github.com/repos/devonfw-forge/tutorials/pulls', function 
     for (var i in json) {
         var e = json[i];
         console.log("Clone " + e.head.repo.clone_url + " -> " + e.head.ref);
-        let cmd = "rm -R playbooks " + 
-        " && echo remote -v && git remote -v " +
-        " && echo remote set-url origin && git remote set-url origin " + e.head.repo.clone_url + " " +
-        " && echo config --list && git config --list " +
+        let cmd = "rm -R playbooks " +
         " && echo clone && git clone " + e.head.repo.clone_url + " playbooks "+
+        " && cd playbooks " + 
+        " && (echo remote -v && git remote -v " +
+        " && echo config --list && git config --list " +
         " && echo remote update && git remote update " +
         " && echo fetch --all && git fetch --all " +
-        " && echo pull --all && git pull --all " +
         " && echo branch -a && git branch -a " +
         " && echo git show-ref && git show-ref " +
         " && branch_name=$(git symbolic-ref -q HEAD) && branch_name=${branch_name##refs/heads/} && branch_name=${branch_name:-HEAD} && echo $branch_name "+
-        " && if [ \"$branch_name\" != \"" + e.head.ref + "\" ]; then echo checkout && git checkout " + e.head.ref + "; fi "+
-        " && echo buildRun && sh buildRun.sh";
+        " && if [ \"$branch_name\" != \"" + e.head.ref + "\" ]; then echo checkout && git checkout " + e.head.ref + "; fi )"+
+        " ; cd .."
+        " && echo buildRun && sh buildRun.sh"
+        ;
         console.log(cmd);
         let cp = child_process.spawnSync(
                     cmd,
