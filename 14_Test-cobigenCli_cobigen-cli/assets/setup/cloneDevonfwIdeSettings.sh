b1e79c229132345afbaa976946fbe242dac98cd7
devonfw <devonfw>
2021-01-15 11:02:03 +0000

    Updated tutorials

--- 14_Test-cobigenCli_cobigen-cli/assets/setup/cloneDevonfwIdeSettings.sh
+++ 14_Test-cobigenCli_cobigen-cli/assets/setup/cloneDevonfwIdeSettings.sh
@@ -8,6 +8,9 @@ git clone https://github.com/devonfw/ide-settings.git settings
 TOOLS="DEVON_IDE_TOOLS=(java mvn)"
 echo $TOOLS > settings/devon.properties
 
+NPM_CONFIG="unsafe-perm=true"
+echo $NPM_CONFIG >> settings/devon/conf/npm/.npmrc
+
 mv /root/devonfw-settings/settings/ /root/devonfw-settings/settings.git
 
 cd /root/devonfw-settings/settings.git
