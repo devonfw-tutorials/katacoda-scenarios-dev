acba94a51a2c530d551008d91acf026db7a7c7cb
devonfw <devonfw>
2021-07-13 13:58:53 +0000

    Updated tutorials

--- 161_Tutorial-devon4jmigrate_devon4j-migrate/assets/setup/restoreWorkspace.sh
+++ 161_Tutorial-devon4jmigrate_devon4j-migrate/assets/setup/restoreWorkspace.sh
@@ -4,8 +4,8 @@ if true; then
     rm -r /root/devonfw/workspaces 
 fi
 
-if ! git clone https://github.com/devonfw-tutorials/workspace-workspace-devon4j-migrate.git /root/devonfw/workspaces; then
-    git clone https://github.com/devonfw-tutorials/workspace-workspace-devon4j-migrate.git /root/devonfw/workspaces 
+if ! git clone https://github.com/devonfw-tutorials/workspace-devon4j-migrate.git /root/devonfw/workspaces; then
+    git clone https://github.com/devonfw-tutorials/workspace-devon4j-migrate.git /root/devonfw/workspaces 
 fi
 
 cd /root/devonfw/workspaces
